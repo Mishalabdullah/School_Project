edc9bb27b56482b7eda78a75d4b4e28d6f09552a
gauthstories <gauthstories@outlook.com>
2022-10-27 16:58:45 +0530

    changes

--- login_page.py
+++ login_page.py
@@ -1,5 +1,12 @@
 #creating a login in page for the application
 #username and password input field
 #submit and close button
-import tkinter
+from tkinter import*
+from PIL import ImageTk,Image
+root=Tk()
+root.title('attendence register')
+root.iconbitmap("C:\Users\Gautham\Desktop\School_Project\.png"git)
+root.geometry("576X323")
+
+
 
